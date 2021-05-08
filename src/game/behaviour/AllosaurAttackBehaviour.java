6806340f4ba57d5c0e0ca5a8fd8f285835bd89b2
Dini <dahm0004@student.monash.edu>
2021-05-08 11:15:59 +0800

    implement AllosaurAttackBehaviour

---
+++ src/game/behaviour/AllosaurAttackBehaviour.java
@@ -0,0 +1,46 @@
+package game.behaviour;
+
+import edu.monash.fit2099.engine.*;
+import game.action.AllosaurEatAction;
+import game.action.AttackAction;
+import game.actor.Dinosaur;
+import game.actor.Stegosaur;
+
+import java.util.List;
+
+public class AllosaurAttackBehaviour extends WanderBehaviour{
+    static final int RADIUS = 40;
+
+    @Override
+    public Action getAction(Actor actor, GameMap map) {
+        List<Exit> exitList = map.locationOf(actor).getExits();
+        Actor nearbyTarget = null;
+        for (Exit exit: exitList){
+            Location location = exit.getDestination();
+            if (location.containsAnActor()){
+                nearbyTarget = location.getActor();
+                if (nearbyTarget instanceof Stegosaur){
+                    break;
+                }
+            }
+        }
+        if (nearbyTarget != null){
+            return new AllosaurEatAction((Dinosaur)nearbyTarget);
+        }
+        return null;
+    }
+
+    public Dinosaur findTargetInRadius(Dinosaur actor, GameMap map){
+        Location location = map.locationOf(actor);
+        Dinosaur target = null;
+        int targetDistance;
+        for(int x: map.getXRange()){
+            for(int y: map.getYRange()){
+                Location targetLocation = map.at(x,y);
+                if (targetLocation.containsAnActor()){
+                    targetDistance = FollowBehaviour.distance(location,targetLocation);
+                }
+            }
+        }
+    }
+}
